2275a6c6c443efbd8b1fb1d7221ec447b4f711fe
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2024-07-18 13:31:13 +0100

    created test api route

--- server/index.js
+++ server/index.js
@@ -3,15 +3,20 @@ import express from "express";
 import mongoose from "mongoose";
 
 import dotenv from "dotenv";
+
+import userRouter from "./routes/user.route.js";
+
 dotenv.config({ path: "../.env" });
 
 const app = express();
 
-mongoose
+app.use("/api/user", userRouter);
+
+/* mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
   })
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(err)); */
 
 app.listen(3000, () => console.log("Listening in PORT: 3000 !!!"));

a4447ae328056e316f59bfee22cced6709ea4e11
Hamza Pasking Akhtar <hamzapaskingakhtar@gmail.com>
2024-07-20 01:57:34 +0100

    added Google OAuth Funcationalty with the help of firebase

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -8,6 +8,8 @@ import {
   signInFailure,
 } from "../redux/user/userSlice";
 
+import OAuth from "../components/OAuth";
+
 const SignIn = () => {
   const [formData, setFormData] = useState({});
   const { loading, error } = useSelector((state) => state.user);
@@ -63,13 +65,14 @@ const SignIn = () => {
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
         >
-          {loading ? "Loading..." : "Sign Up"}
+          {loading ? "Loading..." : "Sign In"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
         <Link to="/sign-up">
-          <span className="text-blue-700">Sign In</span>
+          <span className="text-blue-700">Sign Up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
